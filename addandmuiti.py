bd41c40fa8bd567619dfdc4eb64824a150c01bde
11249a373-art <11249a373@kanchiuniv.ac.in>
2025-11-23 21:40:52 +0530

    sum two numbers and multiply by third

---
+++ addandmuiti.py
@@ -0,0 +1,6 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+c = int(input("Enter the third number: "))
+sum_ab = a + b
+result = sum_ab * c
+print("The result is:", result)
